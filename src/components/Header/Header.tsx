7c168118a82cc7af2dfc8d454664165dd9e8819c
lukaasz555 <lukaszzegar@linkgroup.pl>
2022-11-26 09:34:09 +0100

    header mq

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import React, { FC } from 'react';
 
 const Header: FC = () => {
 	return (
-		<header className='bg-white w-full'>
-			<div className='flex justify-between items-center bg-white w-1/1 min-w-mobile max-w-desktop my-2 mx-auto py-2'>
+		<header className='bg-white w-full shadow-bottom'>
+			<div className='flex justify-between items-center w-1/1 min-w-mobile max-w-desktop my-2 mx-auto py-2 px-5 2xl:px-0'>
 				<h1 className='font-extrabold text-xl py-1.5'>Where in the world?</h1>
 				<p className='font-semibold text-base py-1.5'>Dark Mode</p>
 			</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,10 @@ module.exports = {
 		},
 		screens: {
 			sm: '576px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1440px',
 		},
 		maxWidth: {
 			desktop: '1440px',
@@ -22,7 +26,11 @@ module.exports = {
 		minWidth: {
 			mobile: '350px',
 		},
-		extend: {},
+		extend: {
+			boxShadow: {
+				bottom: '0 4px 20px -14px rgba(0, 0, 0, 0.3)',
+			},
+		},
 	},
 	plugins: [],
 };
