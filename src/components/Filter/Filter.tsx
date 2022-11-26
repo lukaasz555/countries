650bb0edbfe2df8777b040800eb741a5e062e5f8
lukaasz555 <lukaszzegar@linkgroup.pl>
2022-11-26 11:11:47 +0100

    filter - styles

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,17 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const Filter: FC = () => {
+	const [open, setOpen] = useState<boolean>(false);
+	const handleFilter = (e: React.MouseEvent<HTMLButtonElement>) => {
+		const target = e.target as Element;
+		console.log(target.innerHTML);
+	};
 	return (
 		<div className='relative bg-darkBG w-filter flex justify-center items-center px-1.5 bg-white shadow-tool rounded-regular py-2 mt-3 md:mt-0'>
-			<button className='font-sans text-sm flex justify-between items-center w-full px-2'>
+			<button
+				className='font-sans text-sm flex justify-between items-center w-full px-2'
+				onClick={() => {
+					setOpen(!open);
+					console.log('menu on/off');
+				}}>
 				<p>Filter by Region</p>
-				<FontAwesomeIcon icon={faChevronDown} />
+				<FontAwesomeIcon
+					icon={faChevronDown}
+					className={`${open ? 'rotate-180' : 'rotate-0'} transition-all`}
+				/>
 			</button>
-			<ul className='absolute top-full mt-2 bg-white w-full py-2 rounded-regular'>
+			<ul
+				className={`absolute top-full mt-2 bg-white w-full py-2 rounded-regular ${
+					open ? 'scale-y-100' : 'scale-y-0'
+				} origin-top transition-all`}>
 				<li>
-					<button className='w-full text-left px-5 py-0.5 text-sm'>
+					<button
+						className='w-full text-left px-5 py-0.5 text-sm'
+						id='#africa'
+						onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+							handleFilter(e)
+						}>
 						Africa
 					</button>
 				</li>
