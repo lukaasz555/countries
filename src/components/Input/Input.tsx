df3703baf01f0c21e641f714081d759fce72b8bc
lukaasz555 <lukaszzegar@linkgroup.pl>
2022-11-26 19:43:58 +0100

    searching input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,8 +1,28 @@
-import React, { FC } from 'react';
+import React, { Dispatch, FC, SetStateAction, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { CountryProps } from '../CountryCard/CountryCard';
 
-const Input: FC = () => {
+interface InputProps {
+	allCountries: CountryProps[] | [];
+	filtered: CountryProps[] | [];
+	setFiltered: Dispatch<SetStateAction<any>>;
+}
+
+const Input: FC<InputProps> = ({ allCountries, filtered, setFiltered }) => {
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value.toLowerCase();
+		if (value === '') {
+			setFiltered(allCountries);
+		} else {
+			setFiltered(
+				allCountries.filter((country) =>
+					country.name.common.toLowerCase().includes(value)
+				)
+			);
+		}
+	};
+
 	return (
 		<div className='bg-white p-2.5 shadow-tool w-input rounded-regular'>
 			<FontAwesomeIcon icon={faMagnifyingGlass} className='mx-5' />
@@ -10,6 +30,9 @@ const Input: FC = () => {
 				type='text'
 				placeholder='Search for a country...'
 				className='font-sans mr-5 focus:outline-none text-m'
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+					handleInputChange(e)
+				}
 			/>
 		</div>
 	);
